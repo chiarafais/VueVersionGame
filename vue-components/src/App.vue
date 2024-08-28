315a35ac1d0c6a899863923187bb759f9cd95d7e
chiarafais <chiara0915@gmail.com>
2024-08-28 15:27:45 +0200

    first commit

---
+++ vue-components/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <GameComponent />
+  </div>
+</template>
+
+<script>
+import GameComponent from "./components/Game.vue";
+
+export default {
+  components: { GameComponent },
+  setup(props, ctx) {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
